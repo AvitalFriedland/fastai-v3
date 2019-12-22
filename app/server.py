603392228981c34977b67b397489086064a6521d
Avital Friedland <37730178+AvitalFriedland@users.noreply.github.com>
2019-12-22 14:44:34 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -12,7 +12,7 @@ from starlette.staticfiles import StaticFiles
 export_file_url = 'https://drive.google.com/uc?export=download&id=1-9rUnHade3I9lcpaD9tnFSEzPTZvm3UZ'
 export_file_name = 'export.pkl'
 
-classes = ['skiing', 'snowboarding']
+classes = ['ski', 'snowboard']
 path = Path(__file__).parent
 
 app = Starlette()
